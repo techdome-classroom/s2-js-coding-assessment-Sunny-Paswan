1b8ef757c5b6877b2c261b7c5989348b3554aebe
Sunny <mistersunny920@gmail.com>
2024-10-08 14:22:56 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -15,7 +15,26 @@ var isValid = function(s) {
         else if (char === '['){}
         container.push('[')
     }
-        
+    else if (char === ')') {
+        if (stack.length === 0 || stack[stack.length - 1] !== '(') {
+            return false;
+        }
+        stack.pop();  // Pop after confirming a match
+    } else if (char === '}') {
+        if (stack.length === 0 || stack[stack.length - 1] !== '{') {
+            return false;
+        }
+        stack.pop();  // Pop after confirming a match
+    } else if (char === ']') {
+        if (stack.length === 0 || stack[stack.length - 1] !== '[') {
+            return false;
+        }
+        stack.pop();  // Pop after confirming a match
+    }
+}
+
+// If the stack is empty, all brackets were matched
+return stack.length === 0;
 
 };
 
