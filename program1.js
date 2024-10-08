f301ef89add9c6d49321e9f18f372433f337fbe8
Sunny <mistersunny920@gmail.com>
2024-10-08 14:15:23 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -7,7 +7,7 @@ var isValid = function(s) {
     var close = 0;
     for(let i=0; i<s.length; i++){
         if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-            open++:
+            open++;
         }
     }
 };
