0a73c429029b1be3976882230fd4772f0453e41c
Sunny <mistersunny920@gmail.com>
2024-10-08 14:37:45 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,36 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;  // Variable to store the final integer value
+
+    // Loop through the string of Roman numerals
+    for (let i = 0; i < s.length; i++) {
+        // Get the value of the current Roman numeral
+        let currentValue = romanMap[s[i]];
+        
+        // Get the value of the next Roman numeral (if any)
+        let nextValue = romanMap[s[i + 1]];
+
+        // If the current value is less than the next value, subtract the current value
+        if (currentValue < nextValue) {
+            total -= currentValue;
+        } else {
+            // Otherwise, add the current value
+            total += currentValue;
+        }
+    }
+
+    return total;
 };
 
 
