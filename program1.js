389d9a165b8e45e79a146c2fd1c86b31515936d3
Sunny <mistersunny920@gmail.com>
2024-10-08 14:26:46 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,40 +2,35 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    let container = [];
+var isValid = function (s) {
+  let container = [];
 
-    for(let char in s){
-        if (char ==='('){
-        container.push('(');
-        }
-        else if (char === '{'){
-        container.push('{')
-        }
-        else if (char === '['){
-        container.push('[')
+  for (let char in s) {
+    if (char === "(") {
+      container.push("(");
+    } else if (char === "{") {
+      container.push("{");
+    } else if (char === "[") {
+      container.push("[");
+    } else if (char === ")") {
+      if (container.length === 0 || container[container.length - 1] !== "(") {
+        return false;
+      }
+      stack.pop();
+    } else if (char === "}") {
+      if (container.length === 0 || container[container.length - 1] !== "{") {
+        return false;
+      }
+      container.pop();
+    } else if (char === "]") {
+      if (container.length === 0 || container[container.length - 1] !== "[") {
+        return false;
+      }
+      container.pop();
     }
-    else if (char === ')') {
-        if (container.length === 0 || container[container.length - 1] !== '(') {
-            return false;
-        }
-        stack.pop();  
-    } else if (char === '}') {
-        if (container.length === 0 || container[container.length - 1] !== '{') {
-            return false;
-        }
-        container.pop();  
-    } else if (char === ']') {
-        if (container.length === 0 || container[container.length - 1] !== '[') {
-            return false;
-        }
-        container.pop();  
-    }
-    
-return container.length === 0;
-    }
+
+    return container.length === 0;
+  }
 };
 
 module.exports = { isValid };
-
-
